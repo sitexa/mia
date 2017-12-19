95302212fee627ca45d345022d279d3b2bb74d99
xnpeng <xnpeng@hotmail.com>
2017-12-19 09:44:49 +0800

    如果已经登录，就无须启动AppModule(bootstrap),否则重启进入欢迎和登录流程。

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,5 +3,18 @@ import 'meteor-client';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
+import { MeteorObservable } from "meteor-rxjs";
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+Meteor.startup(() => {
+  const subscription = MeteorObservable.autorun().subscribe(() => {
+
+    console.log("Meteor.loggingIn:" + Meteor.loggingIn());
+
+    if (Meteor.loggingIn()) {
+      return;
+    }
+
+    setTimeout(() => subscription.unsubscribe());
+    platformBrowserDynamic().bootstrapModule(AppModule);
+  });
+});
