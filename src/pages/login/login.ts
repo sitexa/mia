30e6e60e3af4096fc8cd65e637e7cecf00776fdf
xnpeng <xnpeng@hotmail.com>
2017-12-22 10:38:20 +0800

    delete unused code lines

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Alert, AlertController, IonicPage, NavController } from 'ionic-angular';
+import { AlertController, IonicPage, NavController } from 'ionic-angular';
 import { PhoneService } from '../../providers/providers';
 import { MainPage } from "../pages";
 
@@ -31,40 +31,6 @@ export class LoginPage {
       }).catch((e) => {
       this.handleError(e);
     })
-
-    /*const alert = this.alertCtrl.create({
-      title: 'Confirm',
-      message: `Would you like to proceed with the phone number ${phone}?`,
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel'
-        },
-        {
-          text: 'Yes',
-          handler: () => {
-            this.handleLogin(alert);
-            return false;
-          }
-        }
-      ]
-    });
-
-    alert.present();*/
-  }
-
-  handleLogin(alert: Alert): void {
-    alert.dismiss().then(() => {
-      return this.phoneService.verify("+86" + this.phone);
-    })
-      .then(() => {
-        this.navCtrl.push('VerificationPage', {
-          phone: "+86" + this.phone
-        });
-      })
-      .catch((e) => {
-        this.handleError(e);
-      });
   }
 
   handleError(e: Error): void {
